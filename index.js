25c4907ccec38b464de361515571b82e513b32d3
Nick Lafauci <nicholas.lafauci@gmail.com>
2023-05-12 13:06:41 -0400

    updates

--- index.js
+++ index.js
@@ -2,46 +2,28 @@ const inventory = newInventory()
 move(inventory).to(0, 0)
 
 const character = newImage('assets/green-character/static.gif')
-let direction = null;
-let x = 100;
-let y = 250;
 
-function moveCharacter(){ 
+function handleDirectionChange(direction){
+    if(direction === null){
+        character.src = `assets/green-character/static.gif`
+    }
     if(direction === 'west'){
-        x = x - 1
+        character.src = `assets/green-character/west.gif`
     }
     if(direction === 'north'){
-        y = y + 1
+        character.src = `assets/green-character/north.gif`
     }
     if(direction === 'east'){
-        x = x + 1
+        character.src = `assets/green-character/east.gif`
     }
     if(direction === 'south'){
-        y = y - 1
+        character.src = `assets/green-character/south.gif`
     }
-    character.style.left = x + 'px'
-    character.style.bottom = y + 'px'
 }
 
-setInterval(function(){ 
-    if(direction === 'west'){
-        x = x - 1
-    }
-    if(direction === 'north'){
-        y = y + 1
-    }
-    if(direction === 'east'){
-        x = x + 1
-    }
-    if(direction === 'south'){
-        y = y - 1
-    }
-    character.style.left = x + 'px'
-    character.style.bottom = y + 'px'
-}, 1)
-                                 
+move(character).withArrowKeys(100, 250, handleDirectionChange)
 
-                                   
+
 
 move(newImage('assets/tree.png')).to(200, 450)
 move(newImage('assets/pillar.png')).to(350, 250)
